c66e523c1a587425e1653d5c64439b3078c78c8e
Kartikmadasanal <117498824+Kartikmadasanal@users.noreply.github.com>
2024-08-29 03:01:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
+
+
+
+
 import express from "express";
+import bodyParser from "body-parser"
+
 import TelegramBot from "node-telegram-bot-api";
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const port = process.env.PORT || 3000;
 const token = '7512867048:AAFDxc8EhxcQGffDHI-xjjYvw1P3_WQUVzE';
 
 const bot = new TelegramBot(token, {polling: true});
 
-app.post("/iot/:lat/:lag", (req, res) => {
-    console.log(req.params);
+app.post("/iot", (req, res) => {
+
+    console.log(req.body);
     
-    // Convert req.params to a readable string format
-    const message = `Received coordinates: Latitude = ${req.params.lat}, Longitude = ${req.params.lag}`;
+    const message = `Received coordinates: Latitude = ${req.body.Latitude}, Longitude = ${req.body.Longitude}`;
     
     // Send message to the specified Telegram user or group
     bot.sendMessage('@jcernew', message)
@@ -26,3 +34,4 @@ app.post("/iot/:lat/:lag", (req, res) => {
 app.listen(port, () => {
     console.log("Server is listening on port " + port);
 });
+
