3c7aac9b2edff258a7ae7c150df8cb318cb9e3ca
Kartikmadasanal <117498824+Kartikmadasanal@users.noreply.github.com>
2024-08-29 02:01:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const token = '7512867048:AAFDxc8EhxcQGffDHI-xjjYvw1P3_WQUVzE';
 
 const bot = new TelegramBot(token, {polling: true});
 
-app.get("/iot/:lat/:lag", (req, res) => {
+app.post("/iot/:lat/:lag", (req, res) => {
     console.log(req.params);
     
     // Convert req.params to a readable string format
